565672a58571a6c496883d6ea7caf80c2e0483d2
kabilz <alikabil2006@gmail.com>
2026-08-03 22:43:21 +0300

    changes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,7 +8,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <div className={styles.card}>
         <div className={styles.logo}>
           <Link href="/">
-            <img src="https://cdn.salla.sa/cdn-cgi/image/fit=scale-down,width=400,height=400,onerror=redirect,format=auto/qnbDd/t6pC5j7rlwHJGYJezjbkPuAmdVtPMK98q2jF54YG.jpg" alt="نحو الرشاقة" />
+            <img src="/logo.png" alt="نحو الرشاقة" />
           </Link>
         </div>
         {children}
